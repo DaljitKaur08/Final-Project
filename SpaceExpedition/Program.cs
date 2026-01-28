03c4ebe3411a437cdfc45fd59605c6e9f192e61e
dj <saini26daljit@gmail.com>
2026-01-28 16:20:41 -0600

    doing program class

--- SpaceExpedition/Program.cs
+++ SpaceExpedition/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace SpaceExpedition
+﻿using System.ComponentModel.Design;
+using System.Xml.Serialization;
+
+namespace SpaceExpedition
 {
     internal class Program
     {
@@ -9,12 +12,9 @@
             Console.WriteLine("Loading Galactic Vault");
             VaultFileManager.LoadVaultFile("galactic_vault.txt", vault);
 
-            for(int i = 0; i < vault.Count; i++)
+            for (int i = 0; i < vault.Count; i++)
             {
-                if (vault.Items[i].DecodedName == "")
-                {
-                    vault.Items[i].DecodedName = Decoder.DecodeArtifactName(vault.Items[i].EncodedName);
-                }
+                vault.Items[i].DecodedName = Decoder.DecodeArtifactName(vault.Items[i].EncodedName);
             }
             VaultAlgorithms.InsertionSort(vault);
             bool running = true;
@@ -27,14 +27,62 @@
                 Console.Write("Please Enter Choice :");
 
                 string choice = Console.ReadLine();
-                if(choice == "1")
+                if (choice == "1")
                 {
-                    Console
+                    Console.Write("Enter artifact file name (without extension):");
+                    string fileName = Console.ReadLine();
+
+                    Artifact newArtifact = VaultFileManager.LoadArtifactFile(fileName + ".txt");
+                    if (newArtifact == null)
+                    {
+                        Console.WriteLine("Artifact file not found or invalid");
+                    }
+                    else
+                    {
+                        newArtifact.DecodedName = Decoder.DecodeArtifactName(newArtifact.EncodedName);
+
+                        int insertPosition;
+                        int found = VaultAlgorithms.BinarySearch(vault, newArtifact.DecodedName, out insertPosition);
+
+                        if (found == -1)
+                        {
+                            VaultAlgorithms.OrderedInsert(vault, newArtifact, insertPosition);
+                            Console.WriteLine("Artifact added");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Artifact already exists");
+                        }
+                    }
+
+                } else if (choice == "2")
+                {
+                    Console.WriteLine("Vault is empty");
+                }
+                else
+                {
+                    Console.WriteLine("\n Inventory");
+                    for (int i = 0; i < vault.Count; i++)
+                    {
+                        Console.WriteLine(vault.Items[i].ToString());
+                    }
                 }
             }
-            
-             
-                
+
+
+             else if (choice == "3")
+            {
+                VaultFileManager.SaveSummary("expedition_summary.txt", vault);
+                Console.WriteLine("Saved.Thank you!");
+                running = false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please Try Again");
+            }
+
+        }
+
         }
     }
 }
